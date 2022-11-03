3c8348ded1ecef4fe4d0acb4ef210fbc5daab334
Corey J Rutt <cjw.rutt@gmail.com>
2022-11-02 18:09:35 -0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,31 +13,46 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Corey J Rutt
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          What does C.R.U.D. Stand For?
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Create &rarr;</h2>
+            <p>
+              <ul>
+                <li>A File</li>
+                <li>A Folder</li>
+              </ul>
+            </p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Read &rarr;</h2>
+            <p>
+              <ul>
+                <li>A File</li>
+              </ul>
+            </p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Update &rarr;</h2>
+            <p>
+              <ul>
+                <li>The File</li>
+                <li>The Folder</li>
+                <li>The File Contents</li>
+              </ul>
+            </p>
           </a>
 
           <a
@@ -46,9 +61,12 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Delete &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              <ul>
+                <li>The File</li>
+                <li>The Folder</li>
+              </ul>
             </p>
           </a>
         </div>
